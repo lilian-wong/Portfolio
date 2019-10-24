822d12b6f6d89173fe48deb74f4d293f17ad9412
lilian-wong <lilianwongai@gmail.com>
2019-10-24 05:49:01 -0400

    updated changes

--- index.js
+++ index.js
@@ -4,18 +4,13 @@
 const sectionContent =[
     {
         id:'about',
-        title:'About me',
-        content:'I am a web developer located in Pennsylvania. I also work as an analyst at a bank. Currently, I am enrolled in the Full Stack Web Development Bootcamp at Bloc. You can find my projects in the Portfolio section. Currently, I am looking for a career change and becoming a Web Developer. I love problem-solving. I am passionate about web development. I also love to bake and work on mini electrical projects.'
+        title:'About Me',
+        content:'I currently work as an analyst at a bank. I recently enrolled in the Full Stack Web Development Bootcamp at Bloc. You can find my projects in the Portfolio section. Currently, I am looking for a career change and becoming a Web Developer. I love problem-solving. I am passionate about web development. I also love to bake and work on mini electrical projects.'
     },
     {
         id:'portfolio',
         title:'Portfolio',
         content:'My projects:'
-    },
-    {
-        id:'contact',
-        title:'Contact Info',
-        content:'Email: lilianwongai@gmail.com'
     }
 ];
 
@@ -25,66 +20,21 @@ let projectsList = [
         title:'Quiz App',
         description:'This project is a quiz app. Users click to start the quiz. There is a total of 10 questions. Users will be prompted one question at a time and must answer one of the multiple choices. In the end, users would get a final score and start a new quiz.',
         tech:'HTML, CSS, and JavaScript',
-        live:'https://github.com/lilian-wong/quiz-app.git',
-        repo:'https://lilian-wong.github.io/quiz-app',
+        live:'https://lilian-wong.github.io/quiz-app/',
+        repo:'https://github.com/lilian-wong/quiz-app',
         image: 'images/quizAppScreen.png'
     }
 ];
 
-// Front page 
+// Set front page 
 function startMain(){
-    $('#title').replaceWith(`<section id=title>
-    <p class="content">Hello, my name is Lilian.</p> 
-    <p>I am an analyst and web developer located in Pennsylvania.</p>`); 
-}
-
-// Load page contents
-function newContent(selectedItem){
-    sectionContent.forEach(function(item){
-        if(item.id===selectedItem){
-            $('#title').replaceWith(`<section id=title>
-            <h2>${item.title}</h2>
-            <p class="content">${item.content}</p>`); 
-        }   
-    }); 
-}
-
-//Toggle content info in top navigation
-function getNavItem(){ 
-    $('.navList li').on('click', function(event){
-        let selectedNavItem = $(this).attr('class');
-        if(selectedNavItem==='main-screen'){
-            refresh();
-        }
-        else{
-            newContent(selectedNavItem);
-            if(selectedNavItem==='portfolio'){
-                addProjects();
-            }
-            else if(selectedNavItem==='contact'){
-                addContact();
-            }
-        }
-    });
-}
-
-//Toggle content info in burger menu 
-function getMenuItem(){
-    $('.menu-dropdown li').on('click', function(event){
-        let selectedMenuItem = $(this).attr('class');
-        if(selectedMenuItem==='main-screen'){   
-            refresh();
-        }
-        else{
-            newContent(selectedMenuItem);
-            if(selectedMenuItem==='portfolio'){
-                addProjects();
-            }
-            else if(selectedMenuItem==='contact'){
-                addContact();
-            }
-        }
-    });
+    $('header').after(`
+    <p class="front_msg">Hello, my name is Lilian.</p> 
+    <p class="front_msg">I am an analyst and web developer located in Pennsylvania.</p>
+    <section class="content" id=${sectionContent[0].id}>
+    <h3>${sectionContent[0].title}</h3>
+    <p>${sectionContent[0].content}</p>
+    </section>` + addProjects());  
 }
 
 function getMenu(){
@@ -101,17 +51,19 @@ function closeMenu(){
 
 // Adding projects content
 function addProjects(){
-    $('#title').append(`<p><strong>${projectsList[0].title}</strong></p>
+    return `
+    <section class="content" id=${sectionContent[1].id}>
+    <h3>${sectionContent[1].title}</h3>
+    <p><h4>${projectsList[0].title}</h4></p>
     <p>Description: ${projectsList[0].description}</p>
-    <p><img src=${projectsList[0].image} class='screenshot' alt=${projectsList[0].title}></p>
-    <span><a href="${projectsList[0].repo}" target="_blank">REPO | </a></span>
-    <span><a href="${projectsList[0].live}" target="_blank">LIVE</a></span> 
-    <p>Technologies used: ${projectsList[0].tech}</p>`);
-}
-
-//Links for contact
-function addContact(){
-    $('#title').append(`<button class="emailButton"><a href="mailto:lilianwongai@gmail.com"> Email Me</a></button>`);
+    <p>Technologies used: ${projectsList[0].tech}</p>
+    <span><a href="${projectsList[0].repo}" target="_blank" class="projectLinks">REPO</a></span>
+    <span><a href="${projectsList[0].live}" target="_blank" class="projectLinks">LIVE</a></span> 
+    <p><iframe id="quizApp" title="Quiz App"
+    title="quizApp" src="https://lilian-wong.github.io/quiz-app/"
+    </iframe></p>
+    </section>`;
+    
 }
 
 //Setting for when scroll button appear on screen
@@ -122,6 +74,7 @@ function setScroll(){
     }
     else{
         topbtn.style.display='none';
+
     }        
     });
 }
@@ -132,20 +85,22 @@ function getTop(){
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;    
     });
-
 }
+
 function refresh(){
-    startMain();
+    $('.main-screen').on('click', function(event){
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;    
+    });
 }
 
 function loadContents(){
     startMain();
-    getNavItem();
     getMenu();
     closeMenu();
-    getMenuItem();
     setScroll();
     getTop();
+    refresh();
 }
 
 $(loadContents);
